491f3ed0530396ad33ca595a8330e5abd0dbc146
anonympins <anonympins@gmail.com>
2025-07-25 06:46:17 +0200

    adding path resolution to packs
    + workflow e-commerce example
    
    fix imports
    
    loading modules is now in engine creation

--- server.js
+++ server.js
@@ -13,11 +13,20 @@ const bench = GameObject.Create("Benchmark");
 const timer = bench.addComponent(BenchmarkTool);
 timer.start();
 
-const engine = Engine.Create();
+
+const engine = await Engine.Create();
+
+if (process.argv.length === 3) {
+    let arg = process.argv[2];
+    if( arg === 'reset-models'){
+        console.log("resetting models");
+        await engine.resetModels();
+    }
+}
 
 const port = process.env.PORT || 7633;
 engine.start(port, async (r) => {
     const logger = engine.getComponent(Logger);
-
+    console.log("Server started on port" + port);
     timer.stop();
 });
