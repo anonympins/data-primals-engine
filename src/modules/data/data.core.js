c7a54a89f4b2b26d992569a2797b826cf55f7d5a
anonympins <anonympins@gmail.com>
2025-08-10 19:56:05 +0200

    split data modules in two parts

---
+++ src/modules/data/data.core.js
@@ -0,0 +1,15 @@
+import NodeCache from "node-cache";
+
+export const modelsCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
+
+export const mongoDBWhitelist = [
+    "$$NOW", "$in", "$eq", "$gt", "$gte", "$in", "$lt", "$lte", "$ne", "$nin", "$type", "$size",
+    "$and", "$not", "$nor", "$or", "$regexMatch", "$find", "$elemMatch", "$filter", "$toString", "$toObjectId",
+    "$concat",
+    '$add', '$subtract', '$multiply', '$divide', '$mod', '$pow', "$sqrt",
+    "$rand",
+    "$abs", '$sin', '$cos', '$tan', '$asin', '$acos', '$atan',
+    "$toDate", "$toBool", "$toString", "$toInt", "$toDouble",
+    "$dateDiff", "$dateSubtract", "$dateAdd", "$dateToString",
+    '$year', '$month', '$week', '$dayOfMonth', '$dayOfWeek', '$dayOfYear', '$hour', '$minute', '$second', '$millisecond'
+];
