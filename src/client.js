0d23596e24c3e854ea693aa2b0a1b16a71eaa2e9
anonympins <anonympins@gmail.com>
2025-11-27 21:10:20 +0100

    client only integration

---
+++ src/client.js
@@ -0,0 +1,4 @@
+// data-primals-engine/client
+// Ce fichier sert de point d'entrée pour les modules qui peuvent être utilisés en toute sécurité côté client (navigateur).
+
+export { Event } from './events.js';
